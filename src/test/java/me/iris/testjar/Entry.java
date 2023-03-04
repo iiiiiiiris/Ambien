b3a01f065f3d205e663cba1eafd563a6d7048261
iris <83561323+iiiiiiiris@users.noreply.github.com>
2023-03-04 17:24:39 -0600

    updated test jar

--- src/test/java/me/iris/testjar/Entry.java
+++ src/test/java/me/iris/testjar/Entry.java
@@ -2,6 +2,13 @@ package me.iris.testjar;
 
 public class Entry {
     public static void main(String[] args) {
+        final int someVar = 78;
+        System.out.println(someVar);
+
         System.out.println("Yellow");
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println(i);
+        }
     }
 }
